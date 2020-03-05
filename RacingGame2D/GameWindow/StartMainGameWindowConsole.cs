9280d26f4c494a89f550ecc34b3019d2980432e5
Стас Медянцев <smedyancev@gmail.com>
2020-03-05 21:42:00 +0300

    Empty MainGameWindow added.

--- RacingGame2D/GameWindow/StartMainGameWindowConsole.cs
+++ RacingGame2D/GameWindow/StartMainGameWindowConsole.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameWindow
+namespace MainGameWindow
 {
-    class Program
+    class StartMainGameWindowConsole
     {
         static void Main(string[] args)
         {
+            using (Game game = new Game(800, 600, "Racing Game"))
+            {
+                game.Run(60f);
+            }
         }
     }
 }
