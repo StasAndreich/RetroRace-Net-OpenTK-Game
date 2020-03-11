ba844a1488537da75624d299762a5c489cf9644c
Стас Медянцев <smedyancev@gmail.com>
2020-03-11 20:34:05 +0300

    Brief-basic structure of the game added

---
+++ RacingGame2D/RGE/BaseClasses/GameObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGE.BaseClasses
+{
+    /// <summary>
+    /// Base class for all entities in RGEngine scenes.
+    /// </summary>
+    public class GameObject
+    {
+
+    }
+}
--- RacingGame2D/RGE/EngineCore.cs
+++ RacingGame2D/RGE/EngineCore.cs
@@ -4,9 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RGE
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace RGEngine
 {
-    public class EngineCore
+    /// <summary>
+    /// Main core class of the engine that controlls all parts
+    /// like Graphics, Physics, Input etc.
+    /// </summary>
+    public class EngineCore : GameWindow
     {
+        public EngineCore()
+        {
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            
+
+            base.OnLoad(e);
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            
+
+            base.OnRenderFrame(e);
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            
+
+            base.OnUpdateFrame(e);
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+        }
     }
 }
