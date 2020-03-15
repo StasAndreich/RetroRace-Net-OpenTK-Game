ec2acbb639dfce4464c4e5f8861eb9b1c0cd83fb
Стас Медянцев <smedyancev@gmail.com>
2020-03-15 22:30:43 +0300

    side texture drawing testing

--- RacingGame2D/GameWindow/Game.cs
+++ RacingGame2D/GameWindow/Game.cs
@@ -17,12 +17,13 @@ namespace MainGameWindow
 
         int VertexBufferObject;
         int VertexArrayObject;
+        int texture;
         Shader shader;
 
         public Game(int width, int height, string title) :
             base(width, height, GraphicsMode.Default, title)
         {
-
+            GL.Enable(EnableCap.Texture2D);
         }
 
 
@@ -45,10 +46,43 @@ namespace MainGameWindow
 
             //shader.Use();
             //// draw func
+            ///
+
+            texture = LoadTexture(@"C:\Users\smedy\source\repos\OOP_CourseProject_StasMedyancev_NET_WinForms_OpenGL\RacingGame2D\Racing\Contents\low-poly-texture.jpg");
 
             base.OnLoad(e);
         }
 
+        // temp loader func.
+        public int LoadTexture(string path)
+        {
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            Bitmap bitmap = new Bitmap(path);
+            System.Drawing.Imaging.BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            bitmap.UnlockBits(data);
+
+            // TextureWrapS is for S axis.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS,
+                (int) TextureWrapMode.Clamp);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT,
+                (int)TextureWrapMode.Clamp);
+
+            // minimize
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
+                (int)TextureMinFilter.Linear);
+            // magnify
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
+                (int)TextureMagFilter.Linear);
+
+            return id;
+        }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
@@ -57,12 +91,23 @@ namespace MainGameWindow
             //GL.BindVertexArray(VertexArrayObject);
             //GL.DrawArrays(PrimitiveType.Triangles, 0, 2);
 
+            // bind and draw in all the primitivw a custom texture
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+
             GL.Begin(PrimitiveType.Quads);
-            GL.Color3(Color.Red);
+            
+            //GL.Color3(Color.Red);
+            GL.TexCoord2(0, 0);
             GL.Vertex2(0, 0);
-            GL.Color3(Color.Green);
+
+            //GL.Color3(Color.Green);
+            GL.TexCoord2(1, 0);
             GL.Vertex2(1, 0);
+
+            GL.TexCoord2(1, 1);
             GL.Vertex2(1, -1);
+
+            GL.TexCoord2(0, 0);
             GL.Vertex2(0.4f, -0.7f);
             GL.End();
 
