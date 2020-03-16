1d66aa5717ec59b91a0476c2406bbab6549b4832
Стас Медянцев <smedyancev@gmail.com>
2020-03-17 00:24:15 +0300

    Some Sprtite class code added sync.

--- RacingGame2D/RGE/Graphics/Sprite.cs
+++ RacingGame2D/RGE/Graphics/Sprite.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RGEngine.Support;
+
 
 namespace RGEngine.Graphics
 {
     /// <summary>
     /// Represents a Sprite object for use in 2D.
     /// </summary>
-    class Sprite
+    public class Sprite
     {
+        /// <summary>
+        /// Stores a loaded 2D texture.
+        /// </summary>
+        public Texture2D Texture2D { get; set; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public float Rotation { get; set; }
+
+
+        public Sprite(Texture2D texture)
+        {
+           this.Texture2D = texture;
+           this.Width = texture.Width;
+           this.Height = texture.Height;
+        }
     }
 }
